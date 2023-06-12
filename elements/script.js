0ae7646a9e84372adb110a52f396a69a7894af79
gaboquerizo <gabrielbaquerizo2000@gmail.com>
2023-06-12 18:30:32 -0500

    Actualización y Re-ubicación de archivos junio 2023

--- elements/script.js
+++
@@ -1,3 +0,0 @@
-console.log("Hola mundo");
-
-console.log("Hello Gabriel");
--- proyects/TheHeaders22/script12142022.js
+++
@@ -1,5 +0,0 @@
-const btnmore = document.querySelector('#btnmore');
-    const linksid = document.querySelector('#links');
-        btnmore.addEventListener('click', (e) => {
-            linksid.classList.toggle("mostrar");
-        });
